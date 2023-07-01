72f5f44ed01dd9702cab8fdc2c8eb52199dbe20c
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-07-02 00:13:06 +0530

    corrected spelling mistake

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
 
             <div className="contact_main">
 
-                <p>I am looking for opportunities, preferrably in product based companies and startups. If you'd like to get in touch, feel free to <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=shobhitsnagpal@gmail.com&su=Let's Connect and Explore Opportunities" target="_blank" rel="noreferrer">send an email</a>.</p>
+                <p>I am looking for opportunities, preferably in product based companies and startups. If you'd like to get in touch, feel free to <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=shobhitsnagpal@gmail.com&su=Let's Connect and Explore Opportunities" target="_blank" rel="noreferrer">send an email</a>.</p>
                 
             </div>
         </div>
