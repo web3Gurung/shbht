6d1d68ec8a15dca38191ca5d0ff6c67abf48dd1d
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-07-19 18:33:54 +0530

    Added new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,13 +6,18 @@ import youtubeCloneImg from '../assets/youtube-clone.png';
 import tagImg from '../assets/tag.png';
 import memoryCardImg from '../assets/memory-card.png';
 import weatherAppImg from '../assets/weather-app.png';
+import palettePixImg from '../assets/palettepix.png';
 
 const Projects = () => {
 
     const [projects, setProjects] = useState([]);
 
     useEffect(() => {
-        setProjects([{name: 'Youtube Clone', description: 'A Youtube clone using React, Rapid API, Firebase where a user can see the home feed, search for videos/channels and watch videos.', img: youtubeCloneImg, website: 'https://youtube-clone-dusky-nine.vercel.app/', repo: 'https://github.com/Shobhit-Nagpal/youtube-clone'},{name: 'Tag', description: 'A photo tagging game where you have to find your favourite game characters. Made using React, React Router and Firebase', img: tagImg, website: 'https://shobhit-nagpal.github.io/tag/', repo: 'https://github.com/Shobhit-Nagpal/tag'} , {name: 'Memory card', description: 'A memory game where you have to choose all characters only once. The charaters are from the Office! Made using React', img: memoryCardImg, website: 'https://shobhit-nagpal.github.io/memory-card/', repo: 'https://github.com/Shobhit-Nagpal/memory-card'}, {name: 'Weather app', description: 'A weather app which pulls data from OpenWeather API and displays the weather of searched location. Made using HTML, CSS and Javascript.', img: weatherAppImg, website: 'https://shobhit-nagpal.github.io/weather-app/', repo: 'https://github.com/Shobhit-Nagpal/weather-app'}])
+        setProjects([{name: 'Palette Pix', description: 'A tool where designers and developers can quickly test out color palettes. Users can change to palettes of their choice or get a random palette.', img: palettePixImg, website: 'https://shobhit-nagpal.github.io/palettepix/', repo: 'https://github.com/Shobhit-Nagpal/palettepix'}, 
+                     {name: 'Youtube Clone', description: 'A Youtube clone using React, Rapid API, Firebase where a user can see the home feed, search for videos/channels and watch videos.', img: youtubeCloneImg, website: 'https://youtube-clone-dusky-nine.vercel.app/', repo: 'https://github.com/Shobhit-Nagpal/youtube-clone'},
+                     {name: 'Tag', description: 'A photo tagging game where you have to find your favourite game characters. Made using React, React Router and Firebase', img: tagImg, website: 'https://shobhit-nagpal.github.io/tag/', repo: 'https://github.com/Shobhit-Nagpal/tag'} , 
+                     {name: 'Memory card', description: 'A memory game where you have to choose all characters only once. The charaters are from the Office! Made using React', img: memoryCardImg, website: 'https://shobhit-nagpal.github.io/memory-card/', repo: 'https://github.com/Shobhit-Nagpal/memory-card'}, 
+                     {name: 'Weather app', description: 'A weather app which pulls data from OpenWeather API and displays the weather of searched location. Made using HTML, CSS and Javascript.', img: weatherAppImg, website: 'https://shobhit-nagpal.github.io/weather-app/', repo: 'https://github.com/Shobhit-Nagpal/weather-app'}])
     }, [])
     return (
         <div className="projects">
